67cae69c7c5c0640f4c464132891c0e669f98220
DESKTOP-AM4-JF <joseph_foley@hotmail.com>
2021-04-11 17:02:55 +0100

    started requirements.txt

--- request_pred.py
+++ request_pred.py
@@ -5,10 +5,12 @@ Uses request lib whilst app is running to get a prediction
 
 import requests
 
-req = requests.get('http://127.0.0.1:8000/')
-print(req.text)
-print()
-
+# =============================================================================
+# req = requests.get('http://127.0.0.1:8000/')
+# print(req.text)
+# print()
+# 
+# =============================================================================
 json_obs = {'datetime':'2012-05-06 12:00:00',
             'season': 2,
             'holiday':0,
@@ -19,5 +21,10 @@ json_obs = {'datetime':'2012-05-06 12:00:00',
             'humidity':35,
             'windspeed':10.2}
 
-req = requests.post('http://127.0.0.1:8000/predict', json=json_obs)
-print(req.text)
+#local
+try:
+    req = requests.post('http://127.0.0.1:8000/predict', json=json_obs)
+    print(req.text)
+    
+except:
+    pass
