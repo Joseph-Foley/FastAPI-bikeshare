118a2f9eb0a970cca43b39163b256a690110a087
DESKTOP-AM4-JF <joseph_foley@hotmail.com>
2021-04-11 16:37:33 +0100

    MPV of FastAPI. can request pred with json

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Apr 11 10:27:57 2021
-
-@author: JF
+Uses FastAPI to deploye the bike share model
 """
 # =============================================================================
 # IMPORTS
 # =============================================================================
 import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel
 import pandas as pd
 import numpy as np
 import pickle
@@ -21,20 +20,47 @@ model_gbm = pickle.load(open('bike_gbm.pkl', 'rb'))
 # =============================================================================
 # INPUT
 # =============================================================================
-json_obs = {'datetime':'2011-05-06 12:00:00',
+json_obs = {'datetime':'2012-05-06 12:00:00',
             'season': 2,
             'holiday':0,
-            'workingday':1,
+            'workingday':0,
             'weather':1,
-            'temp':15.54,
-            'atemp':18.89,
-            'humidity':78,
-            'windspeed':3.1}
+            'temp':28.22,
+            'atemp':30.89,
+            'humidity':35,
+            'windspeed':10.2}
 
 # =============================================================================
-# FUNCTIONS
+# #as BaseModel
+# basey = conditions(datetime='2012-05-06 12:00:00',
+#             season= 2,
+#             holiday=0,
+#             workingday=0,
+#             weather=1,
+#             temp=28.22,
+#             atemp=30.89,
+#             humidity=35,
+#             windspeed=10.2)
+#
+# json_obs == basey.dict()
 # =============================================================================
-def dataPipe(json_obs):
+
+# =============================================================================
+# FUNCTIONS & APP
+# =============================================================================
+class conditions(BaseModel):
+    """Model input via API"""
+    datetime: str
+    season: int
+    holiday: int
+    workingday: int
+    weather: int
+    temp: float
+    atemp: float
+    humidity: int
+    windspeed: float
+
+def dataPipe(json_obs, temp_scaler, temp_PCA, one_hot_encoder):
     """Converts a single json observation into a format that the models is
     expecting"""
     #convert to pandas DF
@@ -65,9 +91,23 @@ def dataPipe(json_obs):
     
     return df
 
+app = FastAPI()
+
+@app.get('/')
+def home():
+    return {'message': 'Hello, Please call predict with expected JSON input'}
+
+@app.post('/predict')
+def predict(data: conditions):
+    data = data.dict()
+    df = dataPipe(data, temp_scaler, temp_PCA, one_hot_encoder)
+    pred = model_gbm.predict(df)[0]
+    return {'prediction': pred}
+
 # =============================================================================
 # PROCESS
 # =============================================================================
-df = dataPipe(json_obs)
-
-preds = model_gbm.predict(df)
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
+    
+#uvicorn app:app --reload    
